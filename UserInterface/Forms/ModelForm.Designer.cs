a6289e9b6805c7ab89a0bf267289399ce24e12f2
DenchikSlaziet <kochetkovdenic1@gmail.com>
2023-04-11 17:33:39 +0300

    Моделирование

--- UserInterface/Forms/ModelForm.Designer.cs
+++ UserInterface/Forms/ModelForm.Designer.cs
@@ -100,7 +100,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(599, 12);
+            this.label1.Location = new System.Drawing.Point(599, 49);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(219, 24);
             this.label1.TabIndex = 3;
@@ -110,7 +110,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(588, 49);
+            this.label2.Location = new System.Drawing.Point(588, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(230, 24);
             this.label2.TabIndex = 4;
